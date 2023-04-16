fe81ce92b0b4cfbec67a1375f43dbf35a54b2406
FireHolls <99182394+FireHolls@users.noreply.github.com>
2023-04-16 15:03:41 +0100

    Add files via upload
    la partie controleur les gars c carré!

---
+++ Controleur.java
@@ -0,0 +1,13 @@
+public class Controleur {
+
+    /**
+     *
+     * une methode principal (main) pour lancer l'application
+     *
+     * @param s
+     */
+    public static void main(String[] s) {
+        // creation de la fenetre
+        menu2 f = new menu2();
+    }
+}
